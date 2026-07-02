88bdd38db83767f59397afbaa310d81205498829
ZXC0428 <happy0426228@gmail.com>
2026-07-02 23:36:57 +0800

    New Annual Report Item 1.3: Organizational Structure (8000–8084) by ZXC.

---
+++ modules/blueprint/dashboard/definition/histology_mapping.py
@@ -0,0 +1,343 @@
+HISTOLOGY_RULES = {
+    "Neoplasm, malignant": {
+        "raw_name": "Neoplasm, malignant",
+        "report_name": "Neoplasm, malignant",
+        "hist": 8000,
+        "behavior": 3
+    },
+    "Tumor cells, malignant": {
+        "raw_name": "Tumor cells, malignant",
+        "report_name": "Tumor cells, malignant",
+        "hist": 8001,
+        "behavior": 3
+    },
+    "Malignant tumor, small cell type": {
+        "raw_name": "Malignant tumor, small cell type",
+        "report_name": "Malignant tumor, small cell type",
+        "hist": 8002,
+        "behavior": 3
+    },
+    "Malignant tumor, giant cell type": {
+        "raw_name": "Malignant tumor, giant cell type",
+        "report_name": "Malignant tumor, giant cell type",
+        "hist": 8003,
+        "behavior": 3
+    },
+    "Malignant tumor, spindle cell type": {
+        "raw_name": "Malignant tumor, spindle cell type",
+        "report_name": "Malignant tumor, spindle cell type",
+        "hist": 8004,
+        "behavior": 3
+    },
+    "Malignant tumor, clear cell type": {
+        "raw_name": "Malignant tumor, clear cell type",
+        "report_name": "Malignant tumor, clear cell type",
+        "hist": 8005,
+        "behavior": 3
+    },
+    "Carcinoma in situ, NOS": {
+        "raw_name": "Carcinoma in situ, NOS",
+        "report_name": "Carcinoma in situ, NOS",
+        "hist": 8010,
+        "behavior": 2
+    },
+    "Carcinoma, NOS": {
+        "raw_name": "Carcinoma, NOS",
+        "report_name": "Carcinoma, NOS",
+        "hist": 8010,
+        "behavior": 3
+    },
+    "Epithelioma, malignant": {
+        "raw_name": "Epithelioma, malignant",
+        "report_name": "Epithelioma, malignant",
+        "hist": 8011,
+        "behavior": 3
+    },
+    "Large cell carcinoma, NOS": {
+        "raw_name": "Large cell carcinoma, NOS",
+        "report_name": "Large cell carcinoma, NOS",
+        "hist": 8012,
+        "behavior": 3
+    },
+    "Large cell neuroendocrine carcinoma": {
+        "raw_name": "Large cell neuroendocrine carcinoma",
+        "report_name": "Large cell neuroendocrine carcinoma",
+        "hist": 8013,
+        "behavior": 3
+    },
+    "Large cell carcinoma with rhabdoid phenotype": {
+        "raw_name": "Large cell carcinoma with rhabdoid phenotype",
+        "report_name": "Large cell carcinoma with rhabdoid phenotype",
+        "hist": 8014,
+        "behavior": 3
+    },
+    "Glassy cell carcinoma": {
+        "raw_name": "Glassy cell carcinoma",
+        "report_name": "Glassy cell carcinoma",
+        "hist": 8015,
+        "behavior": 3
+    },
+    "Carcinoma, undifferentiated, NOS": {
+        "raw_name": "Carcinoma, undifferentiated, NOS",
+        "report_name": "Carcinoma, undifferentiated, NOS",
+        "hist": 8020,
+        "behavior": 3
+    },
+    "Carcinoma, anaplastic, NOS": {
+        "raw_name": "Carcinoma, anaplastic, NOS",
+        "report_name": "Carcinoma, anaplastic, NOS",
+        "hist": 8021,
+        "behavior": 3
+    },
+    "Pleomorphic carcinoma": {
+        "raw_name": "Pleomorphic carcinoma",
+        "report_name": "Pleomorphic carcinoma",
+        "hist": 8022,
+        "behavior": 3
+    },
+    "Nuclear protein in testis (NUT)-associated carcinoma ": {
+        "raw_name": "Nuclear protein in testis (NUT)-associated carcinoma ",
+        "report_name": "Nuclear protein in testis (NUT)-associated carcinoma ",
+        "hist": 8023,
+        "behavior": 3
+    },
+    "Giant cell and spindle cell carcinoma": {
+        "raw_name": "Giant cell and spindle cell carcinoma",
+        "report_name": "Giant cell and spindle cell carcinoma",
+        "hist": 8030,
+        "behavior": 3
+    },
+    "Giant cell carcinoma": {
+        "raw_name": "Giant cell carcinoma",
+        "report_name": "Giant cell carcinoma",
+        "hist": 8031,
+        "behavior": 3
+    },
+    "Spindle cell carcinoma, NOS": {
+        "raw_name": "Spindle cell carcinoma, NOS",
+        "report_name": "Spindle cell carcinoma, NOS",
+        "hist": 8032,
+        "behavior": 3
+    },
+    "Pseudosarcomatous carcinoma": {
+        "raw_name": "Pseudosarcomatous carcinoma",
+        "report_name": "Pseudosarcomatous carcinoma",
+        "hist": 8033,
+        "behavior": 3
+    },
+    "Polygonal cell carcinoma": {
+        "raw_name": "Polygonal cell carcinoma",
+        "report_name": "Polygonal cell carcinoma",
+        "hist": 8034,
+        "behavior": 3
+    },
+    "Carcinoma with osteoclast-like giant cells": {
+        "raw_name": "Carcinoma with osteoclast-like giant cells",
+        "report_name": "",
+        "hist": 8035,
+        "behavior": 3
+    },
+    "Small cell carcinoma, NOS": {
+        "raw_name": "Small cell carcinoma, NOS",
+        "report_name": "Small cell carcinoma, NOS",
+        "hist": 8041,
+        "behavior": 3
+    },
+    "Oat cell carcinoma (C34._)": {
+        "raw_name": "Oat cell carcinoma (C34._)",
+        "report_name": "Oat cell carcinoma ",
+        "hist": 8042,
+        "behavior": 3,
+        "site_include": ["C34"]
+    },
+    "Small cell carcinoma, fusiform cell": {
+        "raw_name": "Small cell carcinoma, fusiform cell",
+        "report_name": "Small cell carcinoma, fusiform cell",
+        "hist": 8043,
+        "behavior": 3
+    },
+    "Small cell carcinoma, intermediate cell": {
+        "raw_name": "Small cell carcinoma, intermediate cell",
+        "report_name": "Small cell carcinoma, intermediate cell",
+        "hist": 8044,
+        "behavior": 3
+    },
+    "Combined small cell carcinoma": {
+        "raw_name": "Combined small cell carcinoma",
+        "report_name": "Combined small cell carcinoma",
+        "hist": 8045,
+        "behavior": 3
+    },
+    "Non-small cell carcinoma (C34._)": {
+        "raw_name": "Non-small cell carcinoma (C34._)",
+        "report_name": "Non-small cell carcinoma",
+        "hist": 8046,
+        "behavior": 3,
+        "site_include": ["C34"]
+    },
+    "Papillary carcinoma in situ": {
+        "raw_name": "Papillary carcinoma in situ",
+        "report_name": "Papillary carcinoma in situ",
+        "hist": 8050,
+        "behavior": 2
+    },
+    "Papillary carcinoma, NOS": {
+        "raw_name": "Papillary carcinoma, NOS",
+        "report_name": "Papillary carcinoma, NOS",
+        "hist": 8050,
+        "behavior": 3
+    },
+    "Warty carcinoma [2018+ CASES FOR C60._, USE 8054/3. ALL OTHER SITES USE 8051/3 FOR ALL YEARS]": {
+        "raw_name": "Warty carcinoma [2018+ CASES FOR C60._, USE 8054/3. ALL OTHER SITES USE 8051/3 FOR ALL YEARS]",
+        "report_name": "Warty carcinoma ",
+        "hist": 8051,
+        "behavior": 3,
+        "conditions": [
+            # 條件一：部位是 C60 且年份小於 2018
+            {
+                "site_include": ["C60"],
+                "year_max": 2017  
+            },
+            # 條件二：排除 C60 的其他任何部位，且無年份限制
+            {
+                "site_exclude": ["C60"]
+            }
+        ]
+    },
+    "Papillary squamous cell carcinoma, non-invasive": {
+        "raw_name": "Papillary squamous cell carcinoma, non-invasive",
+        "report_name": "Papillary squamous cell carcinoma, non-invasive",
+        "hist": 8052,
+        "behavior": 2
+    },
+    "Papillary squamous cell carcinoma": {
+        "raw_name": "Papillary squamous cell carcinoma",
+        "report_name": "Papillary squamous cell carcinoma",
+        "hist": 8052,
+        "behavior": 3
+    },
+    "Warty carcinoma (C60.0-C60.2, C60.9) [2018+. PRE-2018 CASES FOR C60, USE 8051/3.  ALL OTHER SITES USE 8051/3 FOR ALL YEARS]": {
+        "raw_name": "Warty carcinoma (C60.0-C60.2, C60.9) [2018+. PRE-2018 CASES FOR C60, USE 8051/3.  ALL OTHER SITES USE 8051/3 FOR ALL YEARS]",
+        "report_name": "Warty carcinoma ",
+        "hist": 8054,
+        "behavior": 3,
+        "site_include": ["C60"],
+        "year_min": 2018
+    },
+    "Squamous cell carcinoma in situ, NOS": {
+        "raw_name": "Squamous cell carcinoma in situ, NOS",
+        "report_name": "Squamous cell carcinoma in situ, NOS",
+        "hist": 8070,
+        "behavior": 2
+    },
+    "Squamous cell carcinoma, NOS": {
+        "raw_name": "Squamous cell carcinoma, NOS",
+        "report_name": "Squamous cell carcinoma, NOS",
+        "hist": 8070,
+        "behavior": 3
+    },
+    "Differentiated  intraepithelial neoplasia (C60._  ) [2018+ CASES ONLY]": {
+        "raw_name": "Differentiated  intraepithelial neoplasia (C60._  ) [2018+ CASES ONLY]",
+        "report_name": "Differentiated  intraepithelial neoplasia",
+        "hist": 8071,
+        "behavior": 2,
+        "site_include": ["C60"],
+        "year_min": 2018
+    },
+    "Differentiated   intraepithelial neoplasia ( C51._) [2018+ CASES ONLY]": {
+        "raw_name": "Differentiated   intraepithelial neoplasia ( C51._) [2018+ CASES ONLY]",
+        "report_name": "Differentiated   intraepithelial neoplasia",
+        "hist": 8071,
+        "behavior": 2,
+        "site_include": ["C51"],
+        "year_min": 2018
+    },
+    "Squamous cell carcinoma, keratinizing, NOS": {
+        "raw_name": "Squamous cell carcinoma, keratinizing, NOS",
+        "report_name": "Squamous cell carcinoma, keratinizing, NOS",
+        "hist": 8071,
+        "behavior": 3
+    },
+    "Squamous cell carcinoma, large cell, nonkeratinizing, NOS": {
+        "raw_name": "Squamous cell carcinoma, large cell, nonkeratinizing, NOS",
+        "report_name": "Squamous cell carcinoma, large cell, nonkeratinizing, NOS",
+        "hist": 8072,
+        "behavior": 3
+    },
+    "Squamous cell carcinoma, small cell, nonkeratinizing": {
+        "raw_name": "Squamous cell carcinoma, small cell, nonkeratinizing",
+        "report_name": "Squamous cell carcinoma, small cell, nonkeratinizing",
+        "hist": 8073,
+        "behavior": 3
+    },
+    "Squamous cell carcinoma, spindle cell": {
+        "raw_name": "Squamous cell carcinoma, spindle cell",
+        "report_name": "Squamous cell carcinoma, spindle cell",
+        "hist": 8074,
+        "behavior": 3
+    },
+    "Squamous cell carcinoma, adenoid": {
+        "raw_name": "Squamous cell carcinoma, adenoid",
+        "report_name": "Squamous cell carcinoma, adenoid",
+        "hist": 8075,
+        "behavior": 3
+    },
+    "Squamous cell carcinoma in situ with questionable stromal invasion": {
+        "raw_name": "Squamous cell carcinoma in situ with questionable stromal invasion",
+        "report_name": "Squamous cell carcinoma in situ with questionable stromal invasion",
+        "hist": 8076,
+        "behavior": 2
+    },
+    "Squamous cell carcinoma, microinvasive": {
+        "raw_name": "Squamous cell carcinoma, microinvasive",
+        "report_name": "Squamous cell carcinoma, microinvasive",
+        "hist": 8076,
+        "behavior": 3
+    },
+    "Squamous intraepithelial neoplasia, grade III": {
+        "raw_name": "Squamous intraepithelial neoplasia, grade III",
+        "report_name": "Squamous intraepithelial neoplasia, grade III",
+        "hist": 8077,
+        "behavior": 2
+    },
+    "Squamous cell carcinoma with horn formation": {
+        "raw_name": "Squamous cell carcinoma with horn formation",
+        "report_name": "Squamous cell carcinoma with horn formation",
+        "hist": 8078,
+        "behavior": 3
+    },
+    "Queyrat erythroplasia (C60._)": {
+        "raw_name": "Queyrat erythroplasia (C60._)",
+        "report_name": "Queyrat erythroplasia ",
+        "hist": 8080,
+        "behavior": 2,
+        "site_include": ["C60"],
+    },
+    "Bowen disease (C44._)": {
+        "raw_name": "Bowen disease (C44._)",
+        "report_name": "Bowen disease",
+        "hist": 8081,
+        "behavior": 2,
+        "site_include": ["C44"],
+    },
+    "Lymphoepithelial carcinoma": {
+        "raw_name": "Lymphoepithelial carcinoma",
+        "report_name": "Lymphoepithelial carcinoma",
+        "hist": 8082,
+        "behavior": 3
+    },
+    "Basaloid squamous cell carcinoma": {
+        "raw_name": "Basaloid squamous cell carcinoma",
+        "report_name": "Basaloid squamous cell carcinoma",
+        "hist": 8083,
+        "behavior": 3
+    },
+    "Squamous cell carcinoma, clear cell type": {
+        "raw_name": "Squamous cell carcinoma, clear cell type",
+        "report_name": "Squamous cell carcinoma, clear cell type",
+        "hist": 8084,
+        "behavior": 3
+    },
+    
+    
+}
