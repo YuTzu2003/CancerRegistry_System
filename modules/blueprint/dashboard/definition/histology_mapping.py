79b21e9ecc4405f002989b5c9d1b0458d5254bea
ZXC0428 <happy0426228@gmail.com>
2026-07-03 12:56:11 +0800

    New Annual Report Item 1.3: Organizational Structure (8085–8155) by ZXC.

--- modules/blueprint/dashboard/definition/histology_mapping.py
+++ modules/blueprint/dashboard/definition/histology_mapping.py
@@ -133,7 +133,7 @@ HISTOLOGY_RULES = {
     },
     "Carcinoma with osteoclast-like giant cells": {
         "raw_name": "Carcinoma with osteoclast-like giant cells",
-        "report_name": "",
+        "report_name": "Carcinoma with osteoclast-like giant cells",
         "hist": 8035,
         "behavior": 3
     },
@@ -145,7 +145,7 @@ HISTOLOGY_RULES = {
     },
     "Oat cell carcinoma (C34._)": {
         "raw_name": "Oat cell carcinoma (C34._)",
-        "report_name": "Oat cell carcinoma ",
+        "report_name": "Oat cell carcinoma",
         "hist": 8042,
         "behavior": 3,
         "site_include": ["C34"]
@@ -189,7 +189,7 @@ HISTOLOGY_RULES = {
     },
     "Warty carcinoma [2018+ CASES FOR C60._, USE 8054/3. ALL OTHER SITES USE 8051/3 FOR ALL YEARS]": {
         "raw_name": "Warty carcinoma [2018+ CASES FOR C60._, USE 8054/3. ALL OTHER SITES USE 8051/3 FOR ALL YEARS]",
-        "report_name": "Warty carcinoma ",
+        "report_name": "Warty carcinoma",
         "hist": 8051,
         "behavior": 3,
         "conditions": [
@@ -218,11 +218,11 @@ HISTOLOGY_RULES = {
     },
     "Warty carcinoma (C60.0-C60.2, C60.9) [2018+. PRE-2018 CASES FOR C60, USE 8051/3.  ALL OTHER SITES USE 8051/3 FOR ALL YEARS]": {
         "raw_name": "Warty carcinoma (C60.0-C60.2, C60.9) [2018+. PRE-2018 CASES FOR C60, USE 8051/3.  ALL OTHER SITES USE 8051/3 FOR ALL YEARS]",
-        "report_name": "Warty carcinoma ",
+        "report_name": "Warty carcinoma",
         "hist": 8054,
         "behavior": 3,
-        "site_include": ["C60"],
-        "year_min": 2018
+        "year_min": 2018,
+        "site_include": ["C60"]        
     },
     "Squamous cell carcinoma in situ, NOS": {
         "raw_name": "Squamous cell carcinoma in situ, NOS",
@@ -241,16 +241,16 @@ HISTOLOGY_RULES = {
         "report_name": "Differentiated  intraepithelial neoplasia",
         "hist": 8071,
         "behavior": 2,
-        "site_include": ["C60"],
-        "year_min": 2018
+        "year_min": 2018,
+        "site_include": ["C60"]        
     },
     "Differentiated   intraepithelial neoplasia ( C51._) [2018+ CASES ONLY]": {
         "raw_name": "Differentiated   intraepithelial neoplasia ( C51._) [2018+ CASES ONLY]",
         "report_name": "Differentiated   intraepithelial neoplasia",
         "hist": 8071,
         "behavior": 2,
-        "site_include": ["C51"],
-        "year_min": 2018
+        "year_min": 2018,
+        "site_include": ["C51"]        
     },
     "Squamous cell carcinoma, keratinizing, NOS": {
         "raw_name": "Squamous cell carcinoma, keratinizing, NOS",
@@ -308,7 +308,7 @@ HISTOLOGY_RULES = {
     },
     "Queyrat erythroplasia (C60._)": {
         "raw_name": "Queyrat erythroplasia (C60._)",
-        "report_name": "Queyrat erythroplasia ",
+        "report_name": "Queyrat erythroplasia",
         "hist": 8080,
         "behavior": 2,
         "site_include": ["C60"],
@@ -318,7 +318,7 @@ HISTOLOGY_RULES = {
         "report_name": "Bowen disease",
         "hist": 8081,
         "behavior": 2,
-        "site_include": ["C44"],
+        "site_include": ["C44"]
     },
     "Lymphoepithelial carcinoma": {
         "raw_name": "Lymphoepithelial carcinoma",
@@ -338,6 +338,284 @@ HISTOLOGY_RULES = {
         "hist": 8084,
         "behavior": 3
     },
-    
-    
+    "HPV-associated oral epithelial dysplasia, high grade (C02._, C03._, C04._, C05._, C06._) [2026+]":{
+        "raw_name": "HPV-associated oral epithelial dysplasia, high grade (C02._, C03._, C04._, C05._, C06._) [2026+]",
+        "report_name": "HPV-associated oral epithelial dysplasia, high grade",
+        "hist": 8085,
+        "behavior": 2,
+        "year_min": 2026,
+        "site_include": ["C02", "C03", "C04", "C05", "C06"]
+    },
+    "Squamous cell carcinoma, HPV-positive (C01.9, C09.9, C10.2, C10.3, C10.8, C10.9, C31.0–C31.3, C31.9) [2018+]":{
+        "raw_name": "Squamous cell carcinoma, HPV-positive (C01.9, C09.9, C10.2, C10.3, C10.8, C10.9, C31.0–C31.3, C31.9) [2018+]",
+        "report_name": "Squamous cell carcinoma, HPV-positive",
+        "hist": 8085,
+        "behavior": 3,
+        "year_min": 2018,
+        "site_include": ["C019", "C099", "C102", "C103", "C108", "C109", "C310", "C311", "C312", "C313", "C319"]
+    },
+    "Squamous cell carcinoma, HPV-negative (C01.9, C09.9, C10.2, C10.3, C10.8, C10.9, C31.0–C31.3, C31.9) [2018+]":{
+        "raw_name": "Squamous cell carcinoma, HPV-negative (C01.9, C09.9, C10.2, C10.3, C10.8, C10.9, C31.0–C31.3, C31.9) [2018+]",
+        "report_name": "Squamous cell carcinoma, HPV-negative",
+        "hist": 8086,
+        "behavior": 3,
+        "year_min": 2018,
+        "site_include": ["C019", "C099", "C102", "C103", "C108", "C109", "C310", "C311", "C312", "C313", "C319"]
+    },
+    "Basal cell carcinoma, NOS (C44._)":{
+        "raw_name": "Basal cell carcinoma, NOS (C44._)",
+        "report_name": "Basal cell carcinoma, NOS",
+        "hist": 8090,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Superficial basal cell carcinoma (C44._)":{
+        "raw_name": "Superficial basal cell carcinoma (C44._)",
+        "report_name": "Superficial basal cell carcinoma",
+        "hist": 8091,
+        "behavior": 3,        
+        "site_include": ["C44"]
+    },
+    "Infiltrating basal cell carcinoma, NOS (C44._)":{
+        "raw_name": "Infiltrating basal cell carcinoma, NOS (C44._)",
+        "report_name": "Infiltrating basal cell carcinoma, NOS",
+        "hist": 8092,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Basal cell carcinoma, fibroepithelial (C44._)":{
+        "raw_name": "Basal cell carcinoma, fibroepithelial (C44._)",
+        "report_name": "Basal cell carcinoma, fibroepithelial",
+        "hist": 8093,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Basosquamous carcinoma (C44._)":{
+        "raw_name": "Basosquamous carcinoma (C44._)",
+        "report_name": "Basosquamous carcinoma",
+        "hist": 8094,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Metatypical carcinoma (C44._)":{
+        "raw_name": "Metatypical carcinoma (C44._)",
+        "report_name": "Metatypical carcinoma",
+        "hist": 8095,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Basal cell carcinoma, nodular (C44._)":{
+        "raw_name": "Basal cell carcinoma, nodular (C44._)",
+        "report_name": "Basal cell carcinoma, nodular",
+        "hist": 8097,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Adenoid basal carcinoma (C53._)":{
+        "raw_name": "Adenoid basal carcinoma (C53._)",
+        "report_name": "Adenoid basal carcinoma",
+        "hist": 8098,
+        "behavior": 3,
+        "site_include": ["C53"]
+    },
+    "Trichoblastic carcinoma (C44._) [2022+]":{
+        "raw_name": "Trichoblastic carcinoma (C44._) [2022+]",
+        "report_name": "Trichoblastic carcinoma",
+        "hist": 8100,
+        "behavior": 3,
+        "year_min": 2022,
+        "site_include": ["C44"]
+    },
+    "Trichilemmocarcinoma (C44._)":{
+        "raw_name": "Trichilemmocarcinoma (C44._)",
+        "report_name": "Trichilemmocarcinoma",
+        "hist": 8102,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Pilomatrical carcinoma (C44._)":{
+        "raw_name": "Pilomatrical carcinoma (C44._)",
+        "report_name": "Pilomatrical carcinoma",
+        "hist": 8110,
+        "behavior": 3,
+        "site_include": ["C44"]
+    },
+    "Transitional cell carcinoma in situ":{
+        "raw_name": "Transitional cell carcinoma in situ",
+        "report_name": "Transitional cell carcinoma in situ",
+        "hist": 8120,
+        "behavior": 2
+    },
+    "Transitional cell carcinoma, NOS":{
+        "raw_name": "Transitional cell carcinoma, NOS",
+        "report_name": "Transitional cell carcinoma, NOS",
+        "hist": 8120,
+        "behavior": 3
+    },
+    "Schneiderian carcinoma (C30.0, C31._)":{
+        "raw_name": "Schneiderian carcinoma (C30.0, C31._)",
+        "report_name": "Schneiderian carcinoma",
+        "hist": 8121,
+        "behavior": 3,
+        "site_include": ["C300","C31"]
+    },
+    "Urothelial carcinoma, sarcomatoid":{
+        "raw_name": "Urothelial carcinoma, sarcomatoid",
+        "report_name": "Urothelial carcinoma, sarcomatoid",
+        "hist": 8122,
+        "behavior": 3
+    },
+    "Basaloid carcinoma":{
+        "raw_name": "Basaloid carcinoma",
+        "report_name": "Basaloid carcinoma",
+        "hist": 8123,
+        "behavior": 3
+    },
+    "Cloacogenic carcinoma (C21.2)":{
+        "raw_name": "Cloacogenic carcinoma (C21.2)",
+        "report_name": "Cloacogenic carcinoma",
+        "hist": 8124,
+        "behavior": 3,
+        "site_include": ["C212"]
+    },
+    "Papillary transitional cell carcinoma, non-invasive (C67._)":{
+        "raw_name": "Papillary transitional cell carcinoma, non-invasive (C67._)",
+        "report_name": "Papillary transitional cell carcinoma, non-invasive",
+        "hist": 8130,
+        "behavior": 2,
+        "site_include": ["C67"]
+    },
+    "Papillary transitional cell carcinoma (C67._)":{
+        "raw_name": "Papillary transitional cell carcinoma (C67._)",
+        "report_name": "Papillary transitional cell carcinoma",
+        "hist": 8130,
+        "behavior": 3,
+        "site_include": ["C67"]
+    },
+    "Urothelial carcinoma, micropapillary (C67._)":{
+        "raw_name": "Urothelial carcinoma, micropapillary (C67._)",
+        "report_name": "Urothelial carcinoma, micropapillary",
+        "hist": 8131,
+        "behavior": 3,
+        "site_include": ["C67"]
+    },
+    "Adenocarcinoma in situ, NOS":{
+        "raw_name": "Adenocarcinoma in situ, NOS",
+        "report_name": "Adenocarcinoma in situ, NOS",
+        "hist": 8140,
+        "behavior": 2
+    },
+    "Adenocarcinoma, NOS":{
+        "raw_name": "Adenocarcinoma, NOS",
+        "report_name": "Adenocarcinoma, NOS",
+        "hist": 8140,
+        "behavior": 3,
+        "site_exclude": ["C619"]
+    },
+    "Acinar cell carcinoma [EXCLUDES C61.9, USE 8140/3 FOR PROSTATE]":{
+        "raw_name": "Acinar cell carcinoma [EXCLUDES C61.9, USE 8140/3 FOR PROSTATE]",
+        "report_name": "Acinar cell carcinoma",
+        "hist": 8140,
+        "behavior": 3,
+        "site_include": ["C619"]
+    },
+    "Scirrhous adenocarcinoma":{
+        "raw_name": "Scirrhous adenocarcinoma",
+        "report_name": "Scirrhous adenocarcinoma",
+        "hist": 8141,
+        "behavior": 3
+    },
+    "Linitis plastica (C16._)":{
+        "raw_name": "Linitis plastica (C16._)",
+        "report_name": "Linitis plastica",
+        "hist": 8142,
+        "behavior": 3,
+        "site_include": ["C16"]
+    },
+    "Superficial spreading adenocarcinoma":{
+        "raw_name": "Superficial spreading adenocarcinoma",
+        "report_name": "Superficial spreading adenocarcinoma",
+        "hist": 8143,
+        "behavior": 3
+    },
+    "Intestinal-type adenoma, high grade (C16._, C17._) [REPORTABLE 2022+ FOR STOMACH AND SMALL INTESTINE ONLY; SEE STANDARD SETTER REQUIREMENTS FOR REPORTABILITY.]":{
+        "raw_name": "Intestinal-type adenoma, high grade (C16._, C17._) [REPORTABLE 2022+ FOR STOMACH AND SMALL INTESTINE ONLY; SEE STANDARD SETTER REQUIREMENTS FOR REPORTABILITY.]",
+        "report_name": "Intestinal-type adenoma, high grade",
+        "hist": 8144,
+        "behavior": 2,
+        "year_min": 2022,
+        "site_include": ["C16", "C17"]
+    },
+    "Adenocarcinoma, intestinal type (C16._)":{
+        "raw_name": "Adenocarcinoma, intestinal type (C16._)",
+        "report_name": "Adenocarcinoma, intestinal type",
+        "hist": 8144,
+        "behavior": 3,
+        "site_include": ["C16"]
+    },
+    "Carcinoma, diffuse type (C16._)":{
+        "raw_name": "Carcinoma, diffuse type (C16._)",
+        "report_name": "Carcinoma, diffuse type",
+        "hist": 8145,
+        "behavior": 3,
+        "site_include": ["C16"]
+    },
+    "Basal cell adenocarcinoma":{
+        "raw_name": "Basal cell adenocarcinoma",
+        "report_name": "Basal cell adenocarcinoma",
+        "hist": 8147,
+        "behavior": 3
+    },
+    "Glandular intraepithelial neoplasia, grade III":{
+        "raw_name": "Glandular intraepithelial neoplasia, grade III",
+        "report_name": "Glandular intraepithelial neoplasia, grade III",
+        "hist": 8148,
+        "behavior": 2
+    },
+    "Pancreatic neuroendocrine tumor, nonfunctioning (C25._) [REPORTABLE 2021+.]":{
+        "raw_name": "Pancreatic neuroendocrine tumor, nonfunctioning (C25._) [REPORTABLE 2021+.]",
+        "report_name": "Pancreatic neuroendocrine tumor, nonfunctioning",
+        "hist": 8150,
+        "behavior": 3,
+        "year_min": 2021,
+        "site_include": ["C25"]
+    },
+    "Insulinoma, NOS (C25._) [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8151/0]":{
+        "raw_name": "Insulinoma, NOS (C25._) [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8151/0]",
+        "report_name": "Insulinoma, NOS",
+        "hist": 8151,
+        "behavior": 3,
+        "year_min": 2021,
+        "site_include": ["C25"]
+    },
+    "Glucagonoma, NOS (C25._) [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8152/1]":{
+        "raw_name": "Glucagonoma, NOS (C25._) [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8152/1]",
+        "report_name": "Glucagonoma, NOS",
+        "hist": 8152,
+        "behavior": 3,
+        "year_min": 2021,
+        "site_include": ["C25"]
+    },
+    "Gastrinoma, NOS [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8153/1]":{
+        "raw_name": "Gastrinoma, NOS [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8153/1]",
+        "report_name": "Gastrinoma, NOS",
+        "hist": 8153,
+        "behavior": 3,
+        "year_min": 2021,
+        "site_include": ["C25"]
+    },
+    "Mixed pancreatic endocrine and exocrine tumor, malignant (C25._)":{
+        "raw_name": "Mixed pancreatic endocrine and exocrine tumor, malignant (C25._)",
+        "report_name": "Mixed pancreatic endocrine and exocrine tumor, malignant",
+        "hist": 8154,
+        "behavior": 3,
+        "site_include": ["C25"]
+    },
+    "Vipoma, NOS [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8155/1.]":{
+        "raw_name": "Vipoma, NOS [REPORTABLE 2021+. FOR PRE-2021, USE CODE 8155/1.]",
+        "report_name": "Vipoma, NOS",
+        "hist": 8155,
+        "behavior": 3,
+        "year_min": 2021
+    }
 }
